1aca1e15f9695c5957e5e42d71b21fb13f2bd6f8
David Suarez Pascual <dav.sua.pas@gmail.com>
2016-12-03 17:20:31 +0100

    Init

---
+++ SpringSecurity/src/main/java/com/elipcero/springsecurity/ElipceroSpringSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.elipcero.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElipceroSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElipceroSpringSecurityApplication.class, args);
+	}
+}
